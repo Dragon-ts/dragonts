4c84b8e38cc5d86ffffa93e1e4c30314046c55c8
DragonTs <106637358+Dragon-ts@users.noreply.github.com>
2022-07-28 14:33:44 +0800

    Add files via upload

---
+++ js/play.js
@@ -0,0 +1,13 @@
+function play(){
+	document.getElementById("button").innerHTML="点击这里";
+    document.getElementById("button2").innerHTML="";
+}
+
+function play2(){
+    document.getElementById("button").innerHTML="";
+    document.getElementById("button2").innerHTML="点击这里";
+}
+
+function bonus(){
+    window.open("bonus.html")
+}
