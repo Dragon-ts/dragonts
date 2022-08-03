a8bf368f35d2853c344fcd536a26f2a5d72efc30
DragonTs <106637358+Dragon-ts@users.noreply.github.com>
2022-08-03 11:51:18 +0800

    Add files via upload

--- js/DMC.js
+++ js/DMC.js
@@ -1,3 +1,15 @@
 function bladeBattle(){
     window.location.href="bledbattle.html"
 }
+
+function parkourMaster(){
+    window.location.href="parkourmaster.html"
+}
+
+function race(){
+    window.location.href="race++.html"
+}
+
+function darkside(){
+    window.location.href="darkside.html"
+}
