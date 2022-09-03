4275e3da8263ddf4cd44ba12e7db1a816d8d7738
DragonTs <106637358+Dragon-ts@users.noreply.github.com>
2022-09-03 10:57:12 +0800

    Add files via upload

---
+++ js/Dragon.js
@@ -0,0 +1,15 @@
+function help(){
+    window.location.href='help.html'
+}
+
+function update(){
+    window.location.href='update.html'
+}
+
+function soon(){
+    window.location.href='soon.html'
+}
+
+function bug(){
+    window.location.href='bug.html'
+}
